31a4459ffe674f130bea62bcbda83eb34368d5a2
min <minwang.bus@gmail.com>
2021-04-30 14:38:48 -0400

    start reducer selector

--- Todos/frontend/todo_redux.jsx
+++ Todos/frontend/todo_redux.jsx
@@ -2,13 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import configureStore from "./store/store"
 import {receiveTodo, receiveTodos, removeTodo } from "./actions/todo_actions"
-
+import {receiveStep, receiveSteps, removeStep } from "./actions/step_actions"
+import Root from "./components/root"
 
 
 document.addEventListener("DOMContentLoaded", () =>{
     const root = document.getElementById("root")
-    const test = <h1>Todos App</h1>
-    ReactDOM.render(test,root)
+  
     
     
     
@@ -17,6 +17,13 @@ document.addEventListener("DOMContentLoaded", () =>{
     window.receiveTodo = receiveTodo;
     window.receiveTodos = receiveTodos;
     window.removeTodo = removeTodo;
-    window.store = configureStore();
+    window.receiveStep = receiveStep;
+    window.receiveSteps = receiveSteps;
+    window.removeStep = removeStep;
     
+    const store = configureStore();
+    window.store = store;
+    
+    
+    ReactDOM.render(<Root store={store}/>,root)
 })
